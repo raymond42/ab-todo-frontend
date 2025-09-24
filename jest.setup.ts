7843ccf87928051900a9efc1a293fc5ee75d44da
Raymond <raymond42.gr@gmail.com>
2025-09-24 12:49:07 +0200

    feature: drag and drop

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,4 @@
 import "@testing-library/jest-dom";
-import { TextEncoder, TextDecoder } from "text-encoding";
+import { TextEncoder, TextDecoder } from "util";
 
 Object.assign(globalThis, { TextEncoder, TextDecoder });
--- src/components/KanbanColumn.tsx
+++ src/components/KanbanColumn.tsx
@@ -1,17 +1,23 @@
 import KanbanCard from "@/features/todos/components/KanbanCard";
 import { Todo } from "@/features/todos/store/todoStore";
 import { Badge } from "@/components/ui/badge";
+import { useDroppable } from "@dnd-kit/core";
 
 type Props = {
   title: string;
   todos: Todo[];
   icon: React.ReactNode;
-  variant?: "todo" | "onprogress" | "needsreview" | "done";
+  variant: "todo" | "onprogress" | "needsreview" | "done";
 };
 
 export default function KanbanColumn({ title, todos, icon, variant }: Props) {
+  const { setNodeRef } = useDroppable({ id: variant });
+
   return (
-    <div className="bg-white dark:bg-neutral-900 rounded-2xl p-4 flex flex-col gap-3 shadow-sm border border-gray-200 dark:border-gray-800">
+    <div
+      ref={setNodeRef}
+      className="bg-white dark:bg-neutral-900 rounded-2xl p-4 flex flex-col gap-3 shadow-sm border border-gray-200 dark:border-gray-800"
+    >
       <div className="flex justify-between items-center mb-1 rounded-xl shadow-sm border border-gray-200 dark:border-neutral-700 p-1.5 bg-gray-50 dark:bg-neutral-900">
         <h2 className="font-semibold text-gray-800 dark:text-gray-100">
           {title}
